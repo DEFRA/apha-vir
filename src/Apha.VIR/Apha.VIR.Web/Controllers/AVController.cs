b935870ddac868ae2eb6dd965facfc26d3c22071
Ken0212JR <kenneth.rodrigues@atos.net>
2025-08-11 18:04:10 +0530

    feat : Code changes for GetAVNumber screen

---
+++ src/Apha.VIR/Apha.VIR.Web/Controllers/AVController.cs
@@ -0,0 +1,45 @@
+﻿using Apha.VIR.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apha.VIR.Web.Controllers
+{
+    public class AVController : Controller
+    {
+        public IActionResult GetAVNumber()
+        {
+            var model = new GetAVNumberViewModel
+            {
+                RecentAVNumbers = GetRecentAVNumbers()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult GetAVNumber(GetAVNumberViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the valid AV Number
+                // For example, redirect to SubmissionSamples page
+                return RedirectToAction("SubmissionSamples", new { avNumber = model.AVNumber });
+            }
+
+            // If we got this far, something failed; redisplay form
+            model.RecentAVNumbers = GetRecentAVNumbers();
+            return View(model);
+        }
+
+        private List<string> GetRecentAVNumbers()
+        {
+            // This is a dummy implementation. In a real application, you would fetch this from a database or service.
+            return new List<string>
+            {
+                "AV000001-01",
+                "PD0001-01",
+                "SI000001-01",
+                "BN000001-01"
+            };
+        }
+    }
+}
---
+++ src/Apha.VIR/Apha.VIR.Web/Models/GetAVNumberViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apha.VIR.Web.Models
+{
+    public class GetAVNumberViewModel
+    {
+        [Required(ErrorMessage = "AV Number is required.")]
+        [RegularExpression(@"^(AV\d{6}-\d{2}|PD\d{4}-\d{2}|SI\d{6}-\d{2}|BN\d{6}-\d{2})$",
+    ErrorMessage = "Invalid AV Number format. Format should be AV000000-00, PD0000-00, SI000000-00 or BN000000-00.")]
+        [Display(Name = "AV Number")]
+        public string AVNumber { get; set; }
+
+        public List<string> RecentAVNumbers { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public bool HasError => !string.IsNullOrEmpty(ErrorMessage);
+
+        public List<string> LastAVNumbers { get; set; }
+
+        public GetAVNumberViewModel()
+        {
+            RecentAVNumbers = new List<string>();
+        }
+
+
+    }
+}
