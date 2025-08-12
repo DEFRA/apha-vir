cd6b05ba36375925831401ecddd7b770cc367979
AShamra1 <animesh.sharma@eviden.com>
2025-08-12 15:21:46 +0000

    feat: initial report code

---
+++ src/Apha.VIR/Apha.VIR.Web/Models/IsolateDispatchReportModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Apha.VIR.Web.Models;
+
+public class IsolateDispatchReportModel
+{
+    public string AVNumber { get; set; } = null!;
+    public string Nomenclature { get; set; } = null!;
+    public int NoOfAliquots { get; set; }
+    public string? PassageNumber { get; set; }
+    public string? Recipient { get; set; }
+    public string? RecipientName { get; set; }
+    public string? RecipientAddress { get; set; }
+    public string? ReasonForDispatch { get; set; }
+    public DateTime DispatchedDate { get; set; }
+    public Guid? DispatchedBy { get; set; }
+    public string DispatchedByName { get; set; } = null!;
+}
---
+++ src/Apha.VIR/Apha.VIR.Web/Models/IsolateDispatchReportViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apha.VIR.Web.Models
+{
+    public class IsolateDispatchReportViewModel
+    {
+        [Display(Name = "Date From")]
+        [Required(ErrorMessage = "Date From must be entered")]
+        [DataType(DataType.Date)]
+        public DateTime DateFrom { get; set; }
+
+        [Display(Name = "Date To")]
+        [Required(ErrorMessage = "Date To must be entered")]
+        [DataType(DataType.Date)]
+        public DateTime DateTo { get; set; }
+
+        public List<IsolateDispatchReportModel> ReportData { get; set; }
+
+        public IsolateDispatchReportViewModel()
+        {
+            ReportData = new List<IsolateDispatchReportModel>();
+        }
+    }
+}
