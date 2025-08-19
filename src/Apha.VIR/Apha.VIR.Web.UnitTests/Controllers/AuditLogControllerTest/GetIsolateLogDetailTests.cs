66948e5081e87084aa87f1b08d614e3dd9dd0197
AShamra1 <animesh.sharma@eviden.com>
2025-08-19 15:08:05 +0000

    feat: audit test cases

---
+++ src/Apha.VIR/Apha.VIR.Web.UnitTests/Controllers/AuditLogControllerTest/GetIsolateLogDetailTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apha.VIR.Application.DTOs;
+using Apha.VIR.Application.Interfaces;
+using Apha.VIR.Web.Controllers;
+using Apha.VIR.Web.Models.AuditLog;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace Apha.VIR.Web.UnitTests.Controllers.AuditLogControllerTest
+{
+    public class GetIsolateLogDetailTests
+    {
+        private readonly IAuditLogService _auditLogService;
+        private readonly IMapper _mapper;
+        private readonly AuditLogController _controller;
+
+        public GetIsolateLogDetailTests()
+        {
+            _auditLogService = Substitute.For<IAuditLogService>();
+            _mapper = Substitute.For<IMapper>();
+            _controller = new AuditLogController(_auditLogService, _mapper);
+        }
+
+
+        [Fact]
+        public async Task GetIsolateLogDetail_InvalidModelState_ReturnsViewWithEmptyModel()
+        {
+            // Arrange
+            _controller.ModelState.AddModelError("error", "some error");
+
+            // Act
+            var result = await _controller.GetIsolateLogDetail(Guid.NewGuid(), "AV123");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("IsolateAuditLogDetail", viewResult.ViewName);
+            var model = Assert.IsType<AuditIsolateLogDetailsViewModel>(viewResult.Model);
+            Assert.Equal("AV123", model.AVNumber);
+        }
+
+
+        [Fact]
+        public async Task GetIsolateLogDetail_ValidModelState_ReturnsViewWithMappedModel()
+        {
+            // Arrange
+            var logId = Guid.NewGuid();
+            var avNumber = "AV123";
+            var serviceResult = new AuditIsolateLogDetailDTO();  
+            var mappedResult = new AuditIsolateLogDetailsViewModel();
+
+            _auditLogService.GetIsolatLogDetailAsync(logId).Returns(serviceResult);
+            _mapper.Map<AuditIsolateLogDetailsViewModel>(serviceResult).Returns(mappedResult);
+
+            // Act
+            var result = await _controller.GetIsolateLogDetail(logId, avNumber);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("IsolateAuditLogDetail", viewResult.ViewName);
+            var model = Assert.IsType<AuditIsolateLogDetailsViewModel>(viewResult.Model);
+            Assert.Equal(avNumber, model.AVNumber);
+            await _auditLogService.Received(1).GetIsolatLogDetailAsync(logId);
+            _mapper.Received(1).Map<AuditIsolateLogDetailsViewModel>(serviceResult);
+        }
+
+        [Fact]
+        public async Task GetIsolateLogDetail_ServiceThrowsException_ThrowsException()
+        {
+            // Arrange
+            var logId = Guid.NewGuid();
+            var avNumber = "AV123";
+            _auditLogService.GetIsolatLogDetailAsync(logId).Throws(new Exception("Service error"));
+
+            // Act & Assert
+            await Assert.ThrowsAsync<Exception>(() => _controller.GetIsolateLogDetail(logId, avNumber));
+        }
+    }
+}
---
+++ src/Apha.VIR/Apha.VIR.Web.UnitTests/Controllers/AuditLogControllerTest/SearchAuditTests.cs
@@ -0,0 +1,164 @@
+﻿using Apha.VIR.Application.Interfaces;
+using Apha.VIR.Web.Controllers;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Apha.VIR.Web.Models.AuditLog;
+using Apha.VIR.Application.DTOs;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace Apha.VIR.Web.UnitTests.Controllers.AuditLogControllerTest
+{
+    public class SearchAuditTests
+    {
+        private readonly IAuditLogService _auditLogService;
+        private readonly IMapper _mapper;
+        private readonly AuditLogController _controller;
+
+        public SearchAuditTests()
+        {
+            _auditLogService = Substitute.For<IAuditLogService>();
+            _mapper = Substitute.For<IMapper>();
+            _controller = new AuditLogController(_auditLogService, _mapper);
+         }
+
+        [Fact]
+        public async Task SearchAudit_IsNewSearch_ValidModel_ReturnsViewWithViewModel()
+        {
+            // Arrange
+            var searchCriteria = new AuditLogSearchModel
+            {
+                AVNumber = "AV000000-01",
+                DateTimeFrom = DateTime.Now.AddDays(-7),
+                DateTimeTo = DateTime.Now,
+                UserId = "user123"
+            };
+
+            var submissionLogs = new List<AuditSubmissionLogDTO> { new AuditSubmissionLogDTO(), new AuditSubmissionLogDTO() };
+
+            _auditLogService.GetSubmissionLogsAsync(Arg.Any<string>(), Arg.Any<DateTime?>(), Arg.Any<DateTime?>(), Arg.Any<string>())
+            .Returns(submissionLogs);
+
+            var mappedLogs = new List<AuditSubmissionLogModel> { new AuditSubmissionLogModel(), new AuditSubmissionLogModel() };
+            _mapper.Map<IEnumerable<AuditSubmissionLogModel>>(Arg.Any<IEnumerable<object>>()).Returns(mappedLogs);
+
+            // Act
+            ValidateModel(searchCriteria, _controller);
+            var result = await _controller.SearchAudit(searchCriteria, true);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<AuditLogViewModel>(viewResult.Model);
+            Assert.Equal("AuditLog", viewResult.ViewName);
+            Assert.Equal(searchCriteria.AVNumber, model.AVNumber);
+            Assert.Equal(searchCriteria.DateTimeFrom, model.DateTimeFrom);
+            Assert.Equal(searchCriteria.DateTimeTo, model.DateTimeTo);
+            Assert.Equal(searchCriteria.UserId, model.UserId);
+            Assert.False(model.ShowErrorSummary);
+            Assert.True(model.IsNewSearch);
+            Assert.Equal(mappedLogs, model.SubmissionLogs);
+        }
+
+        [Fact]
+        public async Task SearchAudit_IsNewSearch_InvalidModel_ReturnsViewWithErrors()
+        {
+            // Arrange
+            var searchCriteria = new AuditLogSearchModel();
+            _controller.ModelState.AddModelError("AVNumber", "AVNumber is required");
+
+            // Act
+            var result = await _controller.SearchAudit(searchCriteria, true);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<AuditLogViewModel>(viewResult.Model);
+            Assert.Equal("AuditLog", viewResult.ViewName);
+            Assert.False(model.ShowErrorSummary);
+        }
+
+        [Fact]
+        public async Task SearchAudit_NotNewSearch_ValidTempData_ReturnsViewWithIsolateLogs()
+        {
+            // Arrange
+            var searchCriteria = new AuditLogSearchModel
+            {
+                AVNumber = "AV000000-01",
+                DateTimeFrom = DateTime.Now.AddDays(-7),
+                DateTimeTo = DateTime.Now,
+                UserId = "user123"
+            };
+
+            // Use real TempDataDictionary
+            var httpContext = new DefaultHttpContext();
+            var tempDataProvider = Substitute.For<ITempDataProvider>();
+            var tempData = new TempDataDictionary(httpContext, tempDataProvider);
+
+            tempData["SearchCriteria"] = JsonConvert.SerializeObject(searchCriteria);
+
+            _controller.TempData = tempData;
+
+            var isolateLogs = new List<AuditIsolateLogDTO> { new AuditIsolateLogDTO(), new AuditIsolateLogDTO() };
+            _auditLogService.GetIsolatLogsAsync(Arg.Any<string>(), Arg.Any<DateTime?>(), Arg.Any<DateTime?>(), Arg.Any<string>())
+            .Returns(isolateLogs);
+
+            var mappedLogs = new List<AuditIsolateLogModel> { new AuditIsolateLogModel(), new AuditIsolateLogModel() };
+            _mapper.Map<IEnumerable<AuditIsolateLogModel>>(Arg.Any<IEnumerable<object>>()).Returns(mappedLogs);
+
+            // Act
+            var result = await _controller.SearchAudit(new AuditLogSearchModel(), false);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<AuditLogViewModel>(viewResult.Model);
+            Assert.Equal("AuditLog", viewResult.ViewName);
+            Assert.Equal(searchCriteria.AVNumber, model.AVNumber);
+            Assert.Equal(searchCriteria.DateTimeFrom, model.DateTimeFrom);
+            Assert.Equal(searchCriteria.DateTimeTo, model.DateTimeTo);
+            Assert.Equal(searchCriteria.UserId, model.UserId);
+            Assert.False(model.ShowErrorSummary);
+            Assert.False(model.IsNewSearch);
+            Assert.Equal(mappedLogs, model.IsolateLogs);
+        }
+
+        [Fact]
+        public async Task SearchAudit_NotNewSearch_InvalidTempData_ReturnsDefaultView()
+        {
+            // Arrange
+            // Use real TempDataDictionary
+            var httpContext = new DefaultHttpContext();
+            var tempDataProvider = Substitute.For<ITempDataProvider>();
+            var tempData = new TempDataDictionary(httpContext, tempDataProvider);
+            tempData["SearchCriteria"] = null;
+            _controller.TempData = tempData;
+
+            // Act
+            var result = await _controller.SearchAudit(new AuditLogSearchModel(), false);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<AuditLogViewModel>(viewResult.Model);
+            Assert.Equal("AuditLog", viewResult.ViewName);
+            Assert.False(model.ShowErrorSummary);
+            Assert.True(model.ShowDefaultView);
+        }
+
+        private void ValidateModel(object model, Controller controller)
+        {
+            var validationContext = new ValidationContext(model, null, null);
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(model, validationContext, validationResults, true);
+
+            foreach (var validationResult in validationResults)
+            {
+                foreach (var memberName in validationResult.MemberNames)
+                {
+                    controller.ModelState.AddModelError(memberName, validationResult.ErrorMessage);
+                }
+            }
+        }
+
+    }
+}
