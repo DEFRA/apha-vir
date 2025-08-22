b150729d7869c8d28921bbf54a76e73adf7850cf
Ken0212JR <kenneth.rodrigues@atos.net>
2025-08-22 14:01:02 +0530

    feat : Adding UI code for Add/Edit IsoalteDetails

---
+++ src/Apha.VIR/Apha.VIR.Web/Controllers/IsolateDetailsController.cs
@@ -0,0 +1,197 @@
+﻿using Apha.VIR.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apha.VIR.Web.Controllers
+{
+    public class IsolateDetailsController : Controller
+    {
+        public ActionResult AddIsolate()
+        {
+            var virusFamilies = new List<VirusFamily>
+            {
+                new VirusFamily { Id = Guid.NewGuid(), Name = "Orthomyxoviridae" },
+                new VirusFamily { Id = Guid.NewGuid(), Name = "Paramyxoviridae" },
+                new VirusFamily { Id = Guid.NewGuid(), Name = "Coronaviridae" }
+            };
+
+            var personList = new List<Person>
+            {
+                new Person { Id = Guid.NewGuid(), Name = "person1" },
+                new Person { Id = Guid.NewGuid(), Name = "person2" }
+            };
+
+            var viabilityList = new List<Viability>
+            {
+                new Viability { Id = Guid.NewGuid(), Name = "Non Viable" },
+                new Viability { Id = Guid.NewGuid(), Name = "Viable" }
+            };
+
+            var isolationMethods = new List<IsolationMethod>
+            {
+                new IsolationMethod { Id = Guid.NewGuid(), Name = "Both" },
+                new IsolationMethod { Id = Guid.NewGuid(), Name = "Mannual" },
+                new IsolationMethod { Id = Guid.NewGuid(), Name = "Classical" }
+            };
+
+            var virusTypes = new List<VirusType>
+            {
+                new VirusType { Id = Guid.NewGuid(), Name = "Influenza A", FamilyId = virusFamilies[0].Id },
+                new VirusType { Id = Guid.NewGuid(), Name = "Influenza B", FamilyId = virusFamilies[0].Id },
+                new VirusType { Id = Guid.NewGuid(), Name = "Newcastle disease virus", FamilyId = virusFamilies[1].Id },
+                new VirusType { Id = Guid.NewGuid(), Name = "SARS-CoV-2", FamilyId = virusFamilies[2].Id }
+            };
+
+            var freezerList = new List<Freezer>
+            {
+                new Freezer { Id = Guid.NewGuid(), Name = "Avian Viro 1" },
+                new Freezer { Id = Guid.NewGuid(), Name = "AVIU Freezer Corridor Bld 70" },
+                new Freezer { Id = Guid.NewGuid(), Name = "CRL NON Containment" }
+            };
+
+            var trayList = new List<Tray>
+            {
+                new Tray { Id = Guid.NewGuid(), Name = "Avian Viro 1" },
+                new Tray { Id = Guid.NewGuid(), Name = "AVIU Freezer Corridor Bld 70" },
+                new Tray { Id = Guid.NewGuid(), Name = "CRL NON Containment" }
+            };
+
+            var viewModel = new IsolateDetailsViewModel
+            {
+                VirusFamilies = virusFamilies,
+                personList = personList,
+                ViabilityList = viabilityList,
+                IsolationMethods = isolationMethods,
+                VirusTypes = virusTypes,
+                FreezerList = freezerList,
+                TrayList = trayList
+            };
+
+            return View("AddEditIsolate", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddIsolate(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult EditIsolate(Guid Id)
+        {
+            var virusFamilies = new List<VirusFamily>
+            {
+                new VirusFamily { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Orthomyxoviridae" },
+                new VirusFamily { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Paramyxoviridae" },
+                new VirusFamily { Id = Guid.Parse("33333333-3333-3333-3333-333333333333"), Name = "Coronaviridae" }
+            };
+
+            var personList = new List<Person>
+            {
+            new Person { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "person1" },
+            new Person { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "person2" }
+            };
+
+            var viabilityList = new List<Viability>
+            {
+                new Viability { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Non Viable" },
+                new Viability { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Viable" }
+            };
+
+            var isolationMethods = new List<IsolationMethod>
+            {
+                new IsolationMethod { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Both" },
+                new IsolationMethod { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Mannual" },
+                new IsolationMethod { Id = Guid.Parse("22222222-2222-2222-2222-333333333333"), Name = "Classical" }
+            };
+
+            var virusTypes = new List<VirusType>
+            {
+                new VirusType { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Influenza A", FamilyId = virusFamilies[0].Id },
+                new VirusType { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Influenza B", FamilyId = virusFamilies[0].Id },
+                new VirusType { Id = Guid.Parse("11111111-1111-1111-1111-222222222222"), Name = "Newcastle disease virus", FamilyId = virusFamilies[1].Id },
+                new VirusType { Id = Guid.Parse("11111111-1111-1111-1111-333333333333"), Name = "SARS-CoV-2", FamilyId = virusFamilies[2].Id }
+            };
+
+            var freezerList = new List<Freezer>
+            {
+                new Freezer { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Avian Viro 1" },
+                new Freezer { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "AVIU Freezer Corridor Bld 70" },
+                new Freezer { Id = Guid.Parse("22222222-2222-2222-2222-333333333333"), Name = "CRL NON Containment" }
+            };
+
+            var trayList = new List<Tray>
+            {
+                new Tray { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Avian Viro 1" },
+                new Tray { Id = Guid.NewGuid(), Name = "AVIU Freezer Corridor Bld 70" },
+                new Tray { Id = Guid.NewGuid(), Name = "CRL NON Containment" }
+            };
+
+            var viewModel = new IsolateDetailsViewModel
+            {
+                IsolateId = Id,
+                IsoSMSReferenceNumber = "REF1001",
+                VirusFamilyId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                VirusTypeId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                IsolationMethodId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                VirusFamilies = virusFamilies,
+                personList = personList,
+                ViabilityList = viabilityList,
+                IsolationMethods = isolationMethods,
+                VirusTypes = virusTypes,
+                FreezerId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                FreezerList = freezerList,
+                TrayId = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                TrayList = trayList,
+                PreviousCurrentViability = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                PreviousCheckedBy = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                PreviousDateChecked = DateTime.Now,
+                AntiserumProduced = true,
+                AntigenProduced = false,
+                PhylogeneticAnalysis = "Phylogenetic analysis details",
+                MaterialTransferAgreement = true,
+                MTALocation = "India",
+                Comment = "This is a comment for the isolate.",
+                ValidToIssue = true,
+                WhyNotValidToIssue = "This is why the isolate is not valid to issue.",
+                Well = "A1",
+                NoOfAliquots = 5,
+                SampleAvailable = true,
+                FirstViablePassageNumber = 1,
+                DateChecked = DateTime.Now,
+                AVNumber = "AV123456",
+                Nomenclature = "Nomenclature Example"
+               
+
+
+
+            };
+
+            return View("AddEditIsolate", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditIsolate(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+    }
+}
---
+++ src/Apha.VIR/Apha.VIR.Web/Models/IsolateDetailsViewModel.cs
@@ -0,0 +1,99 @@
+﻿namespace Apha.VIR.Web.Models
+{
+    public class IsolateDetailsViewModel
+    {
+
+        public Guid? IsolateId { get; set; }
+        public string AVNumber { get; set; }
+        public string Nomenclature { get; set; }
+        public string CheckedBy { get; set; }
+        public Guid? VirusFamilyId { get; set; }
+        public Guid? VirusTypeId { get; set; }
+        public int? YearOfIsolation { get; set; }
+        public bool IsMixedIsolate { get; set; }
+        public Guid? IsolationMethodId { get; set; }
+        public bool AntiserumProduced { get; set; }
+        public bool AntigenProduced { get; set; }
+        public string PhylogeneticAnalysis { get; set; }
+        public string MTALocation { get; set; }
+        public string Comment { get; set; }
+        public bool ValidToIssue { get; set; }
+        public string WhyNotValidToIssue { get; set; }
+        public Guid? FreezerId { get; set; }
+        public Guid? TrayId { get; set; }
+        public string Viability { get; set; }
+        public DateTime? DateChecked { get; set; }
+
+        public string Well { get; set; }
+        public int NoOfAliquots { get; set; }
+        public bool MaterialTransferAgreement { get; set; }
+        public bool SampleAvailable { get; set; }
+        public int FirstViablePassageNumber { get; set; }
+
+        public List<VirusFamily> VirusFamilies { get; set; }
+        public List<Person> personList { get; set; }
+
+        public List<Viability> ViabilityList { get; set; }
+
+        public List<IsolationMethod> IsolationMethods { get; set; }
+
+        public List<VirusType> VirusTypes { get; set; }
+
+        public List<Freezer> FreezerList { get; set; }
+
+        public List<Tray> TrayList { get; set; }
+
+        public string? IsoSMSReferenceNumber { get; set; }
+
+        public string? PhylogeneticFile { get; set; }
+
+        public Guid PreviousCurrentViability { get; set; }
+        public DateTime? PreviousDateChecked { get; set; }
+
+        public Guid PreviousCheckedBy { get; set; }
+
+    }
+
+    public class VirusFamily
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Viability
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class IsolationMethod
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class VirusType
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid FamilyId { get; set; }
+    }
+
+    public class Freezer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Tray
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
