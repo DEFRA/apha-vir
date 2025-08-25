3f13c312c09491af2f20cb207157c7f9b419d604
Ken0212JR <kenneth.rodrigues@atos.net>
2025-08-25 16:09:33 +0530

    feat : Isolate Characteristics code added for UI

---
+++ src/Apha.VIR/Apha.VIR.Web/Controllers/IsolateCharacteristicsController.cs
@@ -0,0 +1,67 @@
+﻿using Apha.VIR.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apha.VIR.Web.Controllers
+{
+    public class IsolateCharacteristicsController : Controller
+    {
+        public IActionResult Edit(int isolateId)
+        {
+            var model = GetDummyCharacteristics(isolateId);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int isolateId, List<IsolateCharacteristicsViewModel> characteristics)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(characteristics);
+            }
+
+            // TODO: Save characteristics to the database
+            return RedirectToAction(nameof(Index), new { isolateId });
+        }
+
+        public IActionResult Cancel()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        private List<IsolateCharacteristicsViewModel> GetDummyCharacteristics(int isolateId)
+        {
+            return new List<IsolateCharacteristicsViewModel>
+            {
+                new IsolateCharacteristicsViewModel
+                {
+                    Id = 1, IsolateId = isolateId, CharacteristicId = 1,
+                    CharacteristicName = "ICPI",
+                    CharacteristicType = "Text",
+                    Value = "Sample text"
+                },
+                new IsolateCharacteristicsViewModel
+                {
+                    Id = 2, IsolateId = isolateId, CharacteristicId = 2,
+                    CharacteristicName = "ICPI",
+                    CharacteristicType = "Numeric",
+                    Value = "123"
+                },
+                new IsolateCharacteristicsViewModel
+                {
+                    Id = 3, IsolateId = isolateId, CharacteristicId = 3,
+                    CharacteristicName = "PCPII",
+                    CharacteristicType = "YesNo",
+                    Value = "true"
+                },
+                new IsolateCharacteristicsViewModel
+                {
+                    Id = 4, IsolateId = isolateId, CharacteristicId = 4,
+                    CharacteristicName = "Type",
+                    CharacteristicType = "SingleList",
+                    Options = new List<string> { "Option A", "Option B", "Option C" },
+                    Value = "Option B"
+                }
+            };
+        }
+    }
+}
---
+++ src/Apha.VIR/Apha.VIR.Web/Models/IsolateCharacteristicsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Apha.VIR.Web.Models
+{
+  
+    public class IsolateCharacteristicsViewModel
+    {
+        public int Id { get; set; }
+        public int IsolateId { get; set; }
+        public int CharacteristicId { get; set; }
+        public string CharacteristicName { get; set; }
+        public string CharacteristicType { get; set; }
+        public string Value { get; set; }
+        public List<string> Options { get; set; } = new List<string>();
+    }
+}
