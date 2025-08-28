d5bc0cbeff7945aaafecc6966bc2e26e1ca066fd
Ken0212JR <kenneth.rodrigues@atos.net>
2025-08-28 13:58:05 +0530

    feat : Changes for IsolateRelocate for the UI

---
+++ src/Apha.VIR/Apha.VIR.Web/Controllers/IsolateRelocateController.cs
@@ -0,0 +1,116 @@
+﻿using Apha.VIR.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Apha.VIR.Web.Controllers
+{
+    public class IsolateRelocateController : Controller 
+    {
+        public IActionResult Index()
+        {
+            var model = new IsolateRelocateViewModel
+            {
+                Freezers = GetDummyFreezerList(),
+                Trays = GetDummyTrayList(),
+                SearchResults = new List<IsolateRelocation>()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+       public IActionResult Search([FromBody] IsolateRelocateViewModel model)
+        {
+            var results = GetDummySearchResults();
+
+            return PartialView("_SearchResults", results);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Save(IsolateRelocateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Update successful.";
+                return RedirectToAction("Index");
+            }
+            model.Freezers = GetDummyFreezerList();
+            model.Trays = GetDummyTrayList();
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult SelectAll()
+        {
+            var model = new IsolateRelocateViewModel
+            {
+                SearchResults = GetDummySearchResults(),
+                Freezers = GetDummyFreezerList(),
+                Trays = GetDummyTrayList()
+            };
+            foreach (var item in model.SearchResults)
+            {
+                item.IsSelected = true;
+            }
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult UnselectAll()
+        {
+            var model = new IsolateRelocateViewModel
+            {
+                SearchResults = GetDummySearchResults(),
+                Freezers = GetDummyFreezerList(),
+                Trays = GetDummyTrayList()
+            };
+            foreach (var item in model.SearchResults)
+            {
+                item.IsSelected = false;
+            }
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateIsolate(Guid id, string freezer, string tray)
+        {
+            TempData["SuccessMessage"] = $"Isolate {id} updated successfully.";
+            return RedirectToAction("Index");
+        }
+
+        private List<IsolateRelocateSelectListItem> GetDummyFreezerList()
+        {
+            return new List<IsolateRelocateSelectListItem>
+            {
+                new IsolateRelocateSelectListItem { Value = "", Text = "Select Freezer" },
+                new IsolateRelocateSelectListItem { Value = "1", Text = "Freezer 1" },
+                new IsolateRelocateSelectListItem { Value = "2", Text = "Freezer 2" },
+                new IsolateRelocateSelectListItem { Value = "3", Text = "Freezer 3" }
+            };
+        }
+
+        private List<IsolateRelocateSelectListItem> GetDummyTrayList()
+        {
+            return new List<IsolateRelocateSelectListItem>
+            {
+                new IsolateRelocateSelectListItem { Value = "", Text = "Select Tray" },
+                new IsolateRelocateSelectListItem { Value = "1", Text = "Tray 1" },
+                new IsolateRelocateSelectListItem { Value = "2", Text = "Tray 2" },
+                new IsolateRelocateSelectListItem { Value = "3", Text = "Tray 3" }
+            };
+        }
+
+        private List<IsolateRelocation>? GetDummySearchResults()
+        {
+            return new List<IsolateRelocation>
+            {
+                new IsolateRelocation { ID = Guid.NewGuid(), AVNumber = "AV001", Nomenclature = "Isolate 1", FreezerName = "Freezer 1", TrayName = "Tray 1", Well = "A1" },
+                new IsolateRelocation { ID = Guid.NewGuid(), AVNumber = "AV002", Nomenclature = "Isolate 2", FreezerName = "Freezer 1", TrayName = "Tray 2", Well = "B2" },
+                new IsolateRelocation { ID = Guid.NewGuid(), AVNumber = "AV003", Nomenclature = "Isolate 3", FreezerName = "Freezer 2", TrayName = "Tray 1", Well = "C3" }
+            };
+        }
+    }
+
+
+}
---
+++ src/Apha.VIR/Apha.VIR.Web/Models/IsolateRelocateViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Apha.VIR.Web.Models
+
+{
+    public class IsolateRelocateViewModel
+    {
+        public string MinAVNumber { get; set; }
+        public string MaxAVNumber { get; set; }
+        public string SelectedFreezer { get; set; }
+        public string SelectedTray { get; set; }
+
+        public string SelectedNewFreezer { get; set; }
+        public string SelectedNewTray { get; set; }
+        public List<IsolateRelocateSelectListItem> Freezers { get; set; }
+        public List<IsolateRelocateSelectListItem> Trays { get; set; }
+        public List<IsolateRelocation> SearchResults { get; set; }
+
+        public IEnumerable<SelectListItem> FreezerSelectList =>
+            Freezers?.Select(f => new SelectListItem { Value = f.Value, Text = f.Text });
+
+        public IEnumerable<SelectListItem> TraySelectList =>
+            Trays?.Select(t => new SelectListItem { Value = t.Value, Text = t.Text });
+    }
+
+    public class IsolateRelocation
+    {
+        public Guid ID { get; set; }
+        public string AVNumber { get; set; }
+        public string Nomenclature { get; set; }
+        public string FreezerName { get; set; }
+        public string TrayName { get; set; }
+        public string Well { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+    public class IsolateRelocateSelectListItem
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+    }
+}
