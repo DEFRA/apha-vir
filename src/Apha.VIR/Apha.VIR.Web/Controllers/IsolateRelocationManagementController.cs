b87dc1309ea15f1c010a95e25f102d8d08ed3843
rohit2agarwal <rohit.2.agarwal@atos.net>
2025-09-05 13:17:39 +0530

    feat: initial IsolateRelocationManagement

---
+++ src/Apha.VIR/Apha.VIR.Web/Controllers/IsolateRelocationManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apha.VIR.Web.Controllers
+{
+    public class IsolateRelocationManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
