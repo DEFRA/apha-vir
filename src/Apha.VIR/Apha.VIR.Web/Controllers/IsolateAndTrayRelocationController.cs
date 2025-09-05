2d8a16b488d12ce28ab33d0cb8d3d1c5f08cdf21
rohit2agarwal <rohit.2.agarwal@atos.net>
2025-09-05 14:20:05 +0530

    feat: Rename the contoller name

--- src/Apha.VIR/Apha.VIR.Web/Controllers/IsolateAndTrayRelocationController.cs
+++ src/Apha.VIR/Apha.VIR.Web/Controllers/IsolateAndTrayRelocationController.cs
@@ -2,8 +2,9 @@
 
 namespace Apha.VIR.Web.Controllers
 {
+    [Route("Relocation")]
     public class IsolateAndTrayRelocationController : Controller
-    {
+    {        
         public IActionResult Index()
         {
             return View();
