58d7e92d4bd162bc0c50b013f4c4aa602919ca92
qureshatos <quresh.ali@atos.net>
2025-09-12 17:29:59 +0530

    feat: delete functionality added

--- src/Apha.VIR/Apha.VIR.Core/Interfaces/IVirusCharacteristicRepository.cs
+++ src/Apha.VIR/Apha.VIR.Core/Interfaces/IVirusCharacteristicRepository.cs
@@ -6,6 +6,8 @@ namespace Apha.VIR.Core.Interfaces
     public interface IVirusCharacteristicRepository
     {
         Task AddEntryAsync(VirusCharacteristic virusCharacteristic);
+        Task<bool> CheckVirusCharactersticsUsageByIdAsync(Guid id);
+        Task DeleteVirusCharactersticsAsync(Guid id, byte[] lastModified);
         Task<IEnumerable<VirusCharacteristic>> GetAllVirusCharacteristicsAsync();
         Task<PagedData<VirusCharacteristic>> GetAllVirusCharacteristicsAsync(int pageNo, int pageSize);
         Task<IEnumerable<VirusCharacteristic>> GetAllVirusCharacteristicsByVirusTypeAsync(Guid? virusType, bool isAbscent);
--- src/Apha.VIR/Apha.VIR.DataAccess/Repositories/VirusCharacteristicRepository.cs
+++ src/Apha.VIR/Apha.VIR.DataAccess/Repositories/VirusCharacteristicRepository.cs
@@ -1,14 +1,17 @@
-﻿using Apha.VIR.Core.Entities;
+﻿using System.Data;
+using System.Linq;
+using Apha.VIR.Core.Entities;
 using Apha.VIR.Core.Interfaces;
 using Apha.VIR.Core.Pagination;
 using Apha.VIR.DataAccess.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Apha.VIR.DataAccess.Repositories
 {
     public class VirusCharacteristicRepository : RepositoryBase<VirusCharacteristic>, IVirusCharacteristicRepository
-    {        
-        public VirusCharacteristicRepository(VIRDbContext context): base(context)
+    {
+        public VirusCharacteristicRepository(VIRDbContext context) : base(context)
         {
         }
 
@@ -87,6 +90,45 @@ namespace Apha.VIR.DataAccess.Repositories
             @Length ={virusCharacteristic.Length},
             @Index ={virusCharacteristic.CharacteristicIndex},
             @LastModified = {virusCharacteristic.LastModified}");
+        }
+        public async Task DeleteVirusCharactersticsAsync(Guid id, byte[] lastModified)
+        {
+            var parameters = new[]
+            {
+           new SqlParameter("@Id", SqlDbType.UniqueIdentifier) { Value = id },
+           new SqlParameter("@LastModified", SqlDbType.Timestamp) { Value = lastModified },
+        };
+
+            await ExecuteSqlAsync(@"EXEC spVirusCharacteristicDelete @Id,@LastModified", parameters);
+        }
+        public async Task<bool> CheckVirusCharactersticsUsageByIdAsync(Guid id)
+        {
+            int TotalEntries = 0;
+            using (var connection = new SqlConnection(_context.Database.GetConnectionString()))
+            {
+                if (connection.State != ConnectionState.Open)
+                    await connection.OpenAsync();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "spVirusCharacteristicGetUsageById";
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    var param = command.CreateParameter();
+                    param.ParameterName = "@id";
+                    param.Value = id;
+                    command.Parameters.Add(param);
+
+                    using (var result = await command.ExecuteReaderAsync())
+                    {
+                        while (await result.ReadAsync())
+                        {
+                            TotalEntries = Convert.ToInt32(result["Entries"].ToString());
+                        }
+                    }
+                }
+            }
+            return TotalEntries > 0 ? true : false;
         }
     }
 }
