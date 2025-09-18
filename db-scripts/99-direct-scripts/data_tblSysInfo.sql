162bc9ab01a61cc875ffc1a1ec3951e6113b7c6d
jaya-venkatesan <68067621+jaya-venkatesan@users.noreply.github.com>
2025-09-18 12:21:46 +0530

    Feature/update db scripts (#61)
    * add comments
    
    * Feat: Initial Commit
    
    ---------
    
    Co-authored-by: kamleshdaxini-eviden <kamlesh.daxini@eviden.com>

---
+++ db-scripts/99-direct-scripts/data_tblSysInfo.sql
@@ -0,0 +1,24 @@
+-- Optional default for testing in SSMS with SQLCMD mode
+--:setvar ENVIRONMENT Test
+
+-- T-SQL variable to hold the environment
+DECLARE @Environment VARCHAR(20);
+
+-- Assign value from SQLCMD variable
+SET @Environment = '$(ENVIRONMENT)';
+
+-- Insert into sys info table
+INSERT INTO [dbo].[tblSysInfo]
+           ([SystemName]
+           ,[DatabaseVersion]
+           ,[ReleaseDate]
+           ,[Environment]
+           ,[Live]
+           ,[ReleaseNotes])
+SELECT 
+    'VIR-' + @Environment AS SystemName,
+    'SQL 2022' AS DatabaseVersion,
+    GETDATE() AS ReleaseDate,
+    @Environment AS Environment,
+    0 AS Live,
+    @Environment + ' Release' AS ReleaseNotes;
